b1b2f46907eb221b859ebda010fb6dc3b1be09dc
koffi413 <kofficyriaque413@gmail.com>
2024-02-27 08:49:56 +0000

    Page inscription

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    // Ajoutez ici le code pour traiter le formulaire avant l'envoi, si nécessaire
+    // event.preventDefault(); // Utilisez cette ligne pour annuler l'envoi du formulaire par défaut
+});
