1380518e75b1465e373ffa54df77d09640597ce6
koffi413 <kofficyriaque413@gmail.com>
2024-02-29 14:30:35 +0000

    image

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MaisonsRepository;
 
 class AcceuilController extends AbstractController
 {
     #[Route('/', name: 'app_acceuil')]
-    public function index(): Response
+    public function index(MaisonsRepository $maisonsRepository): Response
     {
+        $maisons = $maisonsRepository->findAll();
         return $this->render('acceuil/index.html.twig', [
-            'controller_name' => 'AcceuilController',
+            'maisons' => $maisons,
         ]);
     }
 }
--- src/Controller/MaisonsController.php
+++ src/Controller/MaisonsController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MaisonsRepository;
-use App\Entity\Maisons;
 
 class MaisonsController extends AbstractController
 {
     #[Route('/maisons', name: 'app_maisons')]
     public function index(MaisonsRepository $maisonsRepository): Response
     {
-        $maisons = $maisonsRepository->findByDisponibilite(1);
+        $maisons = $maisonsRepository->findAll();
         return $this->render('maisons/index.html.twig', [
             'maisons' => $maisons,
         ]);
